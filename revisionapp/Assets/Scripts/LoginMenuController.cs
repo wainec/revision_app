e14faa113033d997141bbc922c8e5421cc1d455f
wainec <waine.chan@kidstartnow.com>
2017-07-11 15:24:27 +0800

    Initial Test UI setup

---
+++ revisionapp/Assets/Scripts/LoginMenuController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.SceneManagement;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginMenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	public void goToIndexMenu() {
+		SceneManager.LoadScene("IndexMenu");
+	}
+	// Update is called once per frame
+
+
+}
